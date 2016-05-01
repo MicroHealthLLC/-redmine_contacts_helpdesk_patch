8d5728a9a7f6472d9b5e06dae09e57f3f14009da
bilel-kedidi <bilel.kedidi@gmail.com>
2016-05-01 23:16:53 +0100

    fb

--- lib/rchp/queries_helper_patch.rb
+++ lib/rchp/queries_helper_patch.rb
@@ -14,7 +14,7 @@ module Rchp
 
     module InstanceMethods
       include ContactsHelper
-
+      include HelpdeskHelper
 
       def column_content_with_new_helpdesk(column, issue)
         if column.name.eql?(:last_message) && issue.helpdesk_ticket && issue.is_a?(Issue)
@@ -35,12 +35,12 @@ module Rchp
           issue.ticket_resolve_time
         elsif column.name.eql?(:last_message_date) && issue.is_a?(Issue)
           issue.helpdesk_ticket ? l(:label_helpdesk_ago, time_tag(issue.helpdesk_ticket.last_message_date)) : ""
-        elsif column.name.eql?(:helpdesk_ticket) && issue.helpdesk_ticket && issue.customer && issue.is_a?(Issue)
+        elsif column.name.eql?(:helpdesk_ticket) && issue.customer && issue.is_a?(Issue)
           contact_tag(issue.customer, :size => "32", :type => "avatar", :class => "avatar") +
               content_tag(:div,
                           content_tag(:p, link_to(issue.subject, issue_path(issue)), :class => 'ticket-name') +
                               content_tag(:p, content_tag(:small, issue.description.gsub("(\n|\r)", "").strip.truncate(100)), :class => "ticket-description") +
-                              content_tag(:p, "#{content_tag('span', '', :class => "icon #{HelpdeskHelper.helpdesk_ticket_source_icon(issue.helpdesk_ticket)}", :title => l(:label_note_type_email))} #{l(:label_helpdesk_from)}: #{contact_tag(issue.customer, :type => "plain")}, ".html_safe + l(:label_updated_time, time_tag(issue.helpdesk_ticket.last_message_date)).html_safe, :class => "contact-info"),
+                              content_tag(:p, "#{content_tag('span', '', :class => "icon #{helpdesk_ticket_source_icon(issue.helpdesk_ticket)}", :title => l(:label_note_type_email))} #{l(:label_helpdesk_from)}: #{contact_tag(issue.customer, :type => "plain")}, ".html_safe + l(:label_updated_time, time_tag(issue.helpdesk_ticket.last_message_date)).html_safe, :class => "contact-info"),
                           :class => 'ticket-data')
           # content_tag(:div, content_tag(:span, issue.status.name, :class => "tag-label-color status-#{issue.status.id}" , :style => "background-color: #{tag_color(issue.status.name)}"), :class => "ticket-status")
         elsif column.name.eql?(:vote) && issue.helpdesk_ticket && issue.is_a?(Issue)
